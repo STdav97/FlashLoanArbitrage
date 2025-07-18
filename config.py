ea9365db86cf5a5466b456f68e7cb319f7389e72
STdav97 <stilldav97@gmail.com>
2025-07-18 14:27:30 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-TELEGRAM_TOKEN = "8087230342:AAESHs4dFxIl-iD6wMTOLKXBkQQrkmoSLRo"
-TELEGRAM_CHAT_ID = "1427545674"  # Mets-le en string ou int selon la lib Telegram utilisée
+TELEGRAM_TOKEN = ""
+TELEGRAM_CHAT_ID = ""  # Mets-le en string ou int selon la lib Telegram utilisée
