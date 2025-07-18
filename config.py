3fe2fa231bdb5b8a777ff509c0b27dc9ee8a4f2a
STdav97 <stilldav97@gmail.com>
2025-07-18 19:39:39 +0200

    config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 import os
 TELEGRAM_TOKEN = os.getenv("1427545674")
-print("TELEGRAM_TOKEN:", repr(T8087230342:AAESHs4dFxIl-iD6wMTOLKXBkQQrkmoSLRo))  # Debug ici
+print("TELEGRAM_TOKEN:", repr("T8087230342:AAESHs4dFxIl-iD6wMTOLKXBkQQrkmoSLRo"))  # Debug ici
