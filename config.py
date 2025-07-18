f3545064cbb2b8c68a6552c6db26ca95aa340c27
STdav97 <stilldav97@gmail.com>
2025-07-18 19:42:55 +0200

    config.py

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 import os
-TELEGRAM_TOKEN = os.getenv("1427545674")
-print("TELEGRAM_TOKEN:", repr("T8087230342:AAESHs4dFxIl-iD6wMTOLKXBkQQrkmoSLRo"))  # Debug ici
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
